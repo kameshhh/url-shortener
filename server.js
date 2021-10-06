da8085950be2d5ff1ba5cdc7926b449342fd3385
Kamesh <kameshkumar4602@gmail.com>
2021-10-06 11:32:15 +0530

    fifth commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")();
 const ShortUrl = require('./models/shortUrl')
 const app = express();
 
