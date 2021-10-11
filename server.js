0cd73c466be5a0d8cc9528d605b6eff543f89d66
Kamesh <kameshkumar4602@gmail.com>
2021-10-11 15:52:34 +0530

    db solve

--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const dotenv = require('dotenv');
 
 dotenv.config()
 
-mongoose.connect(process.env.URL_DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, () => {
-    console.log("db connected")
-})
-
+mongoose.connect('mongodb://localhost/urlShortener', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('mongoose connected'))
+    .catch((error) => console.log('Error connecting..'))
 
 
 app.use(express.static('public'));
